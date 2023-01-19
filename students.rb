32332c51989cc9c530a7c7f9a19c2f05158248e6
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-01-19 12:24:14 +0100

    Add rentals

--- students.rb
+++ students.rb
@@ -8,7 +8,7 @@ class Student < Person
     self.classroom = classroom
   end
 
-  def classroom(classroom)
+  def classroom=(classroom)
     @classroom = Classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
