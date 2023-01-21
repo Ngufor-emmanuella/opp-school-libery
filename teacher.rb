407db6b76a55f21a093315376a4b4fb0e26653ae
Ngufor emma <98039632+Ngufor-emmanuella@users.noreply.github.com>
2023-01-21 13:16:44 +0100

    Add changes asked by reviewer

--- teacher.rb
+++ teacher.rb
@@ -4,7 +4,7 @@ class Teacher < Person
   def initialize(age, specialization, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
-     @name = name
+    @name = name
   end
 
   def can_use_services?
