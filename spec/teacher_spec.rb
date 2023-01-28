e877de714334897b9c2c4ebf5a28bfad512136d0
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-01-28 11:14:03 +0430

    fix linter errors

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,18 +1,5 @@
 require_relative '../teacher'
 
-# describe Teacher do
-#   context 'When testing the Teacher class' do
-#     it 'should create a new teacher when teacher class is instantiated' do
-#       teacher = Teacher.new(specialization, name, age)
-#       expect(teacher.specialization).to eql 'Ruby is sweet'
-#       expect(teacher.name).to eql 'Etomas'
-#       expect(teacher.age).to eq (34)
-#     end
-#   end
-# end
-
-# require './teacher'
-
 describe Teacher do
   context 'When testing the Teacher class' do
     age = '40'
@@ -31,9 +18,5 @@ describe Teacher do
     it 'should return parent permission for teacher' do
       expect(teacher.specialization).to eq specialization
     end
-
-    # it 'should return if a teacher can use services is true' do
-    #   expect(teacher.can_use_services?).to eq true
-    # end
   end
 end
